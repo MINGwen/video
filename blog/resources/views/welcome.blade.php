1a49742081eddb53d78315fcfc4086012016e81f
mingwen <L13141228@163.com>
2017-05-19 17:13:09 +0800

    测试video元素

--- blog/resources/views/welcome.blade.php
+++ blog/resources/views/welcome.blade.php
@@ -79,13 +79,14 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    HELLO,土豆是一坨屎
+                    HELLO,大拇指
                 </div>
 
                 <div class="links">
                     <a href="https://laravel.com/docs">Documentation</a>
                     <a href="https://laracasts.com">Laracasts</a>
                     <a href="https://laravel-news.com">News</a>
+                    <a href="/movie">Movies</a>
                     <a href="https://forge.laravel.com">Forge</a>
                     <a href="https://github.com/laravel/laravel">GitHub</a>
                 </div>
